97fe0ad8ce0ac5611e68af096eb00ed1298ea7a3
Alan <alanaa92@gmail.com>
2021-11-26 20:46:09 -0300

    Resume v0.9

--- script/js.js
+++ script/js.js
@@ -1,9 +1,20 @@
-function openMenuSmallDivices() {
-    let viewportWidth = window.innerWidth;
-    if (viewportWidth < 1000) {
-
-        document.querySelector(".wrapper-menu").classList.toggle("menu-animation")
 
 
-    }
+
+function sendToHyperLink(url) {
+
+    window.open(url)
+}
+
+const nodeArticle = document.querySelectorAll("main > section > article")
+const arraySections = Array.from(nodeArticle);
+
+const nodeIcons = document.querySelectorAll(".wrapper-menu > Ion-Icon")
+const arrayIcons = Array.from(nodeIcons);
+
+function scrollToArticle(element) {
+
+    const index = arrayIcons.indexOf(element)
+    arraySections[index].scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })
+
 }
