0816b51853deff94fb0e68dcffa918b9792f3dd4
Alan <alanaa92@gmail.com>
2021-11-26 16:01:14 -0300

    proof-of-concept v0.3

--- script/js.js
+++ script/js.js
@@ -1,19 +1,9 @@
+function openMenuSmallDivices() {
+    let viewportWidth = window.innerWidth;
+    if (viewportWidth < 1000) {
+
+        document.querySelector(".wrapper-menu").classList.toggle("menu-animation")
 
 
-
-
-let darkMode = (element) => {
-    document.querySelector("body").classList.toggle("dark-mode")
-    Array.from(element.children).forEach(element => { toggleHidden(element) });
+    }
 }
-
-let toggleHidden = (element) => { element.classList.toggle('hidden') }
-
-
-let scrollToElement = (element) => {
-    element.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })
-}
-
-scrollToElement(document.querySelector(".about-me"))
-
-
